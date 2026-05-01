60086dc8d7650fd90796a8b3befcb52f9d9c7dc2
Dark-Wombat <sumit9977686849@gmail.com>
2026-05-02 01:13:07 +0530

    Add files via upload

---
+++ name_age_storage.py
@@ -0,0 +1,68 @@
+import datetime
+import json
+import os
+import 
+
+path = 'data1.json'
+
+
+def check():
+	if not os.path.exists(path):
+		with open(path, 'w') as file:
+			file.write('[]')
+
+
+def add():
+	with open(path, 'r') as file:
+		data = json.load(file)
+
+	name = input('\nEnter Name: ')
+	age = input('Enter Age: ')
+	info = {"name": name, "age": age}
+
+	data.append(info)
+
+	with open(path, 'w') as file:
+		json.dump(data, file, indent=4)
+		print('Added Successfully!')
+
+
+def view():
+	with open(path, 'r') as file:
+		data = json.load(file)
+
+	for el in data:
+		print()
+		for key, value in el.items():
+				print(f'{key} - {value}')
+
+
+def main():
+	while True:
+		check()
+		print('\n================')
+		print('  DATA PROGRAM')
+		print('================')
+		print('1. Add Entry')
+		print('2. View Entries')
+		print('3. Exit')
+		print('================')
+
+		inp = input('Enter Choice: ')
+
+		if inp == '1':
+			add()
+
+		elif inp == '2':
+			view()
+
+		elif inp == '3':
+			print('\nExiting...')
+			break
+
+		else:
+				print('\nInvalid Input!')
+
+
+main()
+
